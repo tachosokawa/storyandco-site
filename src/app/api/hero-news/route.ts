44b1c18291d1264f33a5aefd330314c2f3d1af4f
tachosokawa <36651193+tachosokawa@users.noreply.github.com>
2026-03-13 20:42:11 +0900

    Create route.ts

---
+++ src/app/api/hero-news/route.ts
@@ -0,0 +1,15 @@
+import { client } from '@/lib/microcms'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  try {
+    const data = await client.getList({
+      endpoint: 'news',
+      queries: { limit: 3, orders: '-publishedAt' },
+      customRequestInit: { next: { revalidate: 60 } }
+    })
+    return NextResponse.json(data.contents)
+  } catch {
+    return NextResponse.json([])
+  }
+}
