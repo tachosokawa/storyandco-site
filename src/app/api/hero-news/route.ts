eacc1b2b2c9b3b5d4f07608f5c0277cdc45b4170
tachosokawa <36651193+tachosokawa@users.noreply.github.com>
2026-03-13 22:30:54 +0900

    Update route.ts

--- src/app/api/hero-news/route.ts
+++ src/app/api/hero-news/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
   try {
     const data = await client.getList({
       endpoint: 'news',
-      queries: { limit: 3, orders: '-publishedAt' },
+      queries: { limit: 3, orders: '-publishDate' },
       customRequestInit: { next: { revalidate: 60 } }
     })
     return NextResponse.json(data.contents)
