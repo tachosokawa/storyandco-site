c891636e8273ef15dd360672fa575b0a2f4a33ef
tachosokawa <36651193+tachosokawa@users.noreply.github.com>
2026-03-13 22:04:32 +0900

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -80,7 +80,6 @@ export default function HeroSection() {
           </p>
           <div className='border-r border-[#2d2a24] h-full'></div>
           <div className="flex-1 overflow-hidden">
-           <div className="flex-1 overflow-hidden">
             <a href={current ? `/news/${current.id}` : '#'} className='block w-full font-bold text-sm cursor-pointer px-3 truncate hover:text-[#18bed7]'>
               {current ? current.title : ''}
             </a>
