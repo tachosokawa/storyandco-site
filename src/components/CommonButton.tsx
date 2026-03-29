ecedbecce969852ea2aff4f6c19af925eb65b967
Tac Hosokawa <takuhosokawa@gmail.com>
2026-03-29 22:17:06 +0900

    Fix contact submit button click area

--- src/components/CommonButton.tsx
+++ src/components/CommonButton.tsx
@@ -8,25 +8,21 @@ interface CommonButtonProps {
   className?: string
 }
 
-export default function CommonButton({ 
-  buttonText, 
+export default function CommonButton({
+  buttonText,
   type,
   className = 'px-[40px] pt-[28px] pb-[30px] border-t border-[#2d2a24] hover:bg-[#18bed7] text-[#333] hover:text-[#FFF] hover:cursor-pointer text-[16px]'
 }: CommonButtonProps) {
   const [isHovered, setIsHovered] = useState(false)
   return (
-    <div 
-      className={className}
+    <button
+      type={type || 'button'}
+      className={`${className} inline-flex w-full items-center transition-all justify-between font-bold`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <button
-        type={type || 'button'}
-        className="inline-flex w-full items-center transition-colors group transition-all justify-between font-bold"
-      >
-        {buttonText}
-        <img src={isHovered ? "/images/clients/arrow-long-right-white.svg" : "/images/clients/arrow-long-right.svg"} alt="arrow" className='w-[16px] md:w-[20px]'/>
-      </button>
-    </div>
+      {buttonText}
+      <img src={isHovered ? "/images/clients/arrow-long-right-white.svg" : "/images/clients/arrow-long-right.svg"} alt="arrow" className='w-[16px] md:w-[20px]'/>
+    </button>
   )
 }
