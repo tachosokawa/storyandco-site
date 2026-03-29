ce1f529e15afcf35057fc87e1df801ef3896acb2
Tac Hosokawa <takuhosokawa@gmail.com>
2026-03-29 20:33:10 +0900

    Fix CommonLink click area to cover full button bar

--- src/components/CommonLink.tsx
+++ src/components/CommonLink.tsx
@@ -9,21 +9,21 @@ interface CommonLinkProps {
   className?: string
 }
 
-export default function CommonLink({ 
-  linkText, 
+export default function CommonLink({
+  linkText,
   href = '/company',
-  className = 'px-[20px] md:px-[40px] pt-[24px] md:pt-[28px] pb-[25px] md:pb-[30px] border-t border-[#2d2a24] md:border-t-0 hover:bg-[#18bed7] text-[#333] hover:text-[#FFF] hover:cursor-pointer text-[14px] md:text-[16px]'
+  className = 'border-t border-[#2d2a24] md:border-t-0 text-[14px] md:text-[16px]'
 }: CommonLinkProps) {
   const [isHovered, setIsHovered] = useState(false)
   return (
-    <div 
+    <div
       className={className}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <Link
         href={href}
-        className="inline-flex w-full items-center transition-colors group transition-all justify-between font-bold leading-[1]"
+        className="flex w-full items-center transition-colors group transition-all justify-between font-bold leading-[1] px-[20px] md:px-[40px] pt-[24px] md:pt-[28px] pb-[25px] md:pb-[30px] hover:bg-[#18bed7] text-[#333] hover:text-[#FFF] hover:cursor-pointer"
       >
         {linkText}
         <img src={isHovered ? "/images/clients/arrow-long-right-white.svg" : "/images/clients/arrow-long-right.svg"} alt="arrow" className='w-[16px] md:w-[20px]'/>
